0cbca5831545c21797a3bcb7494f563ec7ed66fa
attakei <attakei@gmail.com>
2016-05-15 02:40:09 +0900

    Refactor: Add docstrings

--- errcron/cronjob.py
+++ errcron/cronjob.py
@@ -4,9 +4,20 @@
 
 
 class CronJob(object):
+    """Job runner for errcron, handling job running trigger and action
+    """
     def __init__(self):
-        self.trigger_time = None
         self.trigger_format = None
+        """datetime format by trigger to run job"""
+        self.trigger_time = None
+        """datetime value by trigger to run job"""
 
     def is_runnable(self, time):
+        """Check whether job run action at specified time
+
+        :param time: Time to run action
+        :type time: datetime.datetime
+        :return: Job is runnable or not
+        :rtype: boolean
+        """
         return time.strftime(self.trigger_format) == self.trigger_time
