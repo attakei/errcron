221b2238c3facdfc2dbb19ec1ea535a26f849bb3
attakei <attakei@gmail.com>
2016-05-17 03:10:45 +0900

    Init errcron.bot.CrontabMixin

---
+++ errcron/bot.py
@@ -0,0 +1,20 @@
+# -*- coding:utf8 -*-
+"""Bot class extensions
+"""
+
+
+class CrontabMixin(object):
+    """Mix-in class to implement crontab features
+
+    If you will use crontab by it, call activate_crontab
+    """
+    def activate_crontab(self):
+        """Activate polling function and register first crontab
+        """
+        pass
+
+
+    def poll_crontab(self):
+        """Check crontab and run target jobs
+        """
+        pass
