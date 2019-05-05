ef596b43e696f43122758f661eef60e4457d9cd1
Kazuya Takei <attakei@gmail.com>
2019-05-06 01:18:49 +0900

    Add test: admin only can see list of crontab

--- errcron/crontab.py
+++ errcron/crontab.py
@@ -1,7 +1,8 @@
+from errcron.bot import CrontabMixin
 from errbot import BotPlugin, botcmd
 
 
-class Crontab(BotPlugin):
-    @botcmd
+class Crontab(CrontabMixin, BotPlugin):
+    @botcmd(admin_only=True)
     def crontab_list(self, message, args):
         return 'List of cronjobs'
--- tests/test_botplugin.py
+++ tests/test_botplugin.py
@@ -2,13 +2,22 @@
 """
 from pathlib import Path
 
+import pytest
 
 pytest_plugins = ['errbot.backends.test']
 
-
 extra_plugin_dir = str(Path(__file__).parents[1] / 'errcron')
 
+extra_config = {
+    'BOT_ADMINS': ('admin', )
+}
 
-def test_command(testbot):
+
+@pytest.mark.parametrize('sender,expect', [
+    ('user', 'This command requires bot-admin privileges'),
+    ('admin', 'List of cronjobs'),
+])
+def test_list_is_admin_only(sender, expect, testbot):
+    testbot.bot.sender = testbot.bot.build_identifier(sender)
     testbot.push_message('!crontab_list')
-    assert 'List of cronjobs' in testbot.pop_message()
+    assert  expect == testbot.pop_message()
