be210e85e00b5e788cf9411968167897a19ade96
Kazuya Takei <attakei@gmail.com>
2019-05-06 01:50:51 +0900

    Define list command

--- errcron/crontab.py
+++ errcron/crontab.py
@@ -5,4 +5,8 @@ from errbot import BotPlugin, botcmd
 class Crontab(CrontabMixin, BotPlugin):
     @botcmd(admin_only=True)
     def crontab_list(self, message, args):
-        return 'List of cronjobs'
+        head = 'List of cronjobs:'
+        if len(self._crontab) == 0:
+            return f'{head}\n- Not found'
+        job_list = [f'- {job}' for job in self._crontab]
+        return head + '\n' + '\n'.join(job_list)
--- tests/test_botplugin.py
+++ tests/test_botplugin.py
@@ -4,6 +4,10 @@ from pathlib import Path
 
 import pytest
 
+from errcron.cronjob import CronJob
+from errcron.crontab import Crontab
+
+
 pytest_plugins = ['errbot.backends.test']
 
 extra_plugin_dir = str(Path(__file__).parents[1] / 'errcron')
@@ -15,9 +19,21 @@ extra_config = {
 
 @pytest.mark.parametrize('sender,expect', [
     ('user', 'This command requires bot-admin privileges'),
-    ('admin', 'List of cronjobs'),
+    ('admin', 'List of cronjobs:\n- Not found'),
 ])
 def test_list_is_admin_only(sender, expect, testbot):
     testbot.bot.sender = testbot.bot.build_identifier(sender)
     testbot.push_message('!crontab_list')
     assert  expect == testbot.pop_message()
+
+
+def test_list_jobs(testbot):
+    plugin: Crontab = testbot._bot.plugin_manager.get_plugin_obj_by_name('Crontab')
+    plugin._crontab.append(CronJob())
+    testbot.bot.sender = testbot.bot.build_identifier('admin')
+    testbot.push_message('!crontab_list')
+    actual = testbot.pop_message()
+    expect = 'List of cronjobs:\n'
+    assert expect in actual
+    assert len(actual.split('\n')) == 2
+    assert 'CronJob(trigger=[])' in actual
